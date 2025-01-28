d1568f23d61c374b8cd636a69605d3093e645157
Genevieve <awavieve@gmail.com>
2025-01-27 19:46:10 -0500

    added project files

--- FashionVote/obj/Debug/net8.0/FashionVote.AssemblyInfo.cs
+++ FashionVote/obj/Debug/net8.0/FashionVote.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FashionVote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad948804868171c0a74da8c7c67d2caf217d787")]
 [assembly: System.Reflection.AssemblyProductAttribute("FashionVote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FashionVote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
