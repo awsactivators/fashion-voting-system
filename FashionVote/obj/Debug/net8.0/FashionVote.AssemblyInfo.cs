f83ea70a0efd97bdc0531e896ffcd960cd75310b
Genevieve <awavieve@gmail.com>
2025-02-17 18:58:17 -0500

    Add styling to participant view pages

--- FashionVote/obj/Debug/net8.0/FashionVote.AssemblyInfo.cs
+++ FashionVote/obj/Debug/net8.0/FashionVote.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FashionVote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb280e91c163ea539ff3bdeda249fd6052102bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edeca427b0fd6092c5d9b1e468ef1f825af9520f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FashionVote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FashionVote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
